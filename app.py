d514616b0837944d50f340cd90f210f9bd701ba8
dvrkdev <dvrk.devx@gmail.com>
2026-03-18 12:22:59 +0500

    user model created

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, request, url_for
 from flask_babel import Babel, _
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
 
@@ -24,6 +25,21 @@ db = SQLAlchemy(app)
 babel = Babel(app)
 
 
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    date_joined = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    tasks = db.relationship(
+        "Todo", backref="author", lazy=True, cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"<User {self.id}>"
+
+
 class Todo(db.Model):
     __tablename__ = "tasks"
     id = db.Column(db.Integer, primary_key=True)
@@ -31,6 +47,8 @@ class Todo(db.Model):
     completed = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     def __repr__(self):
         return f"<Task {self.id}>"
